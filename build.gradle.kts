db4225275b6a309131cb9d2d92d6ff88ed106740
Helmy <mohamed0132525715@gmail.com>
2021-09-24 16:30:23 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+    java
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
